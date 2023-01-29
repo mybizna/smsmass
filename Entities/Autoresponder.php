6654bc06c16c46de62bf7fbe923c1df38ae9720b
Dedan Irungu <dedanirungu@gmail.com>
2023-01-29 18:21:51 +0300

    setting up all required files and setting up the schema.

--- Entities/Autoresponder.php
+++ Entities/Autoresponder.php
@@ -2,13 +2,14 @@
 
 namespace Modules\Smsmass\Entities;
 
-use Modules\Base\Entities\BaseModel;
 use Illuminate\Database\Schema\Blueprint;
+use Modules\Base\Entities\BaseModel;
 
 class Autoresponder extends BaseModel
 {
 
-    protected $fillable = ['name'];
+    protected $fillable = ['subject', 'body', 'wait_period', 'table_name', 'email_field',
+        'date_field', 'start_date', 'end_date', 'published'];
     public $migrationDependancy = [];
     protected $table = "smsmass_autoresponder";
 
@@ -22,5 +23,14 @@ class Autoresponder extends BaseModel
     {
         $table->increments('id');
         $table->string('name');
+        $table->string('subject');
+        $table->string('body');
+        $table->integer('wait_period');
+        $table->char('table_name', 255);
+        $table->char('email_field', 255);
+        $table->char('date_field', 255);
+        $table->datetime('start_date');
+        $table->datetime('end_date');
+        $table->tinyInteger('published')->default(true);
     }
 }
