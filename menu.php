2ddfdbd3cd7d426a19cbcfb2b5fcba8b4ab94428
Dedan Irungu <dedanirungu@gmail.com>
2023-02-27 15:23:04 +0300

    Adding SMS and Email and Implementing Notification

---
+++ menu.php
@@ -0,0 +1,9 @@
+<?php
+
+$this->add_menu("sms", "smsmass", "Mass Sms", "/smsmass/admin/autoresponder", "fas fa-cogs", 3);
+
+$this->add_submenu("sms", "smsmass", "Autoresponder", "/smsmass/admin/autoresponder", 5);
+$this->add_submenu("sms", "smsmass", "Campaign", "/smsmass/admin/campaign", 5);
+$this->add_submenu("sms", "smsmass", "Group", "/smsmass/admin/group", 5);
+
+
