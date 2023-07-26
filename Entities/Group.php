a9cf0bc03df1caacc98f4aa8d66ec67575f2ffe9
Dedan Irungu <dedanirungu@gmail.com>
2023-07-26 14:00:23 +0300

    adding listtable and formbuilder fuction to all model so that data can be rendered via json

--- Entities/Group.php
+++ Entities/Group.php
@@ -5,6 +5,9 @@ namespace Modules\Smsmass\Entities;
 use Illuminate\Database\Schema\Blueprint;
 use Modules\Base\Entities\BaseModel;
 
+use Modules\Core\Classes\Views\ListTable;
+use Modules\Core\Classes\Views\FormBuilder;
+
 class Group extends BaseModel
 {
 
@@ -12,6 +15,43 @@ class Group extends BaseModel
     public $migrationDependancy = [];
     protected $table = "smsmass_group";
 
+
+    public function listTable(){
+        // listing view fields
+        $fields = new ListTable();
+
+        $fields->name('name')->type('text')->ordering(true);
+        $fields->name('ordering')->type('text')->ordering(true);
+        $fields->name('published')->type('switch')->ordering(true);
+
+        return $fields;
+
+    }
+    
+    public function formBuilder(){
+        // listing view fields
+        $fields = new FormBuilder();
+
+        $fields->name('name')->type('text')->group('w-1/2');
+        $fields->name('description')->type('text')->group('w-1/2');
+        $fields->name('ordering')->type('text')->group('w-1/2');
+        $fields->name('published')->type('switch')->group('w-1/2');
+
+        return $fields;
+
+    }
+
+    public function filter(){
+        // listing view fields
+        $fields = new FormBuilder();
+
+        $fields->name('name')->type('text')->group('w-1/6');
+        $fields->name('ordering')->type('text')->group('w-1/6');
+        $fields->name('published')->type('switch')->group('w-1/6');
+
+        return $fields;
+
+    }
     /**
      * List of fields for managing postings.
      *
