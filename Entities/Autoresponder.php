99a7fba28371c7e38089865d06b6eeb1678b5507
Dedan <dedanirungu@gmail.com>
2024-04-12 11:15:29 +0300

    Add rights() method to Autoresponder, Campaign, and Group entities

--- Entities/Autoresponder.php
+++ Entities/Autoresponder.php
@@ -85,6 +85,12 @@ class Autoresponder extends BaseModel
      */
     public function rights(): array
     {
+        $rights = parent::rights();
 
+        $rights['staff'] = ['view' => true];
+        $rights['registered'] = ['view' => true];
+        $rights['guest'] = [];
+
+        return $rights;
     }
 }
--- Entities/Campaign.php
+++ Entities/Campaign.php
@@ -77,6 +77,12 @@ class Campaign extends BaseModel
      */
     public function rights(): array
     {
+        $rights = parent::rights();
 
+        $rights['staff'] = ['view' => true];
+        $rights['registered'] = ['view' => true];
+        $rights['guest'] = [];
+
+        return $rights;
     }
 }
